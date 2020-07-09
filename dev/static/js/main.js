c76a28ab78b38434ef0ac87d4b65c7eec7b58451
esmolyakov <smolyakov@tecman.ru>
2020-07-09 18:12:42 +0300

    first page

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,82 +1,45 @@
 $(document).ready(function () {
   svg4everybody({});
-});
-
-$('select.dropdown').each(function () {
-
-  var dropdown = $('<div />').addClass('dropdown selectDropdown');
-
-  $(this).wrap(dropdown);
-
-  var label = $('<span />').text($(this).attr('placeholder')).insertAfter($(this));
-  var list = $('<ul />');
-
-  $(this).find('option').each(function () {
-    list.append($('<li />').append($('<a />').text($(this).text())));
+  $('[data-toggle="datepicker"]').datepicker({
+    language: 'ru-Ru'
   });
-
-  list.insertAfter($(this));
-
-  if ($(this).find('option:selected').length) {
-    label.text($(this).find('option:selected').text());
-    list.find('li:contains(' + $(this).find('option:selected').text() + ')').addClass('active');
-    $(this).parent().addClass('filled');
-  }
-
 });
 
-$(document).on('click touch', '.selectDropdown ul li a', function (e) {
-  e.preventDefault();
-  var dropdown = $(this).parent().parent().parent();
-  var active = $(this).parent().hasClass('active');
-  var label = active ? dropdown.find('select').attr('placeholder') : $(this).text();
-
-  dropdown.find('option').prop('selected', false);
-  dropdown.find('ul li').removeClass('active');
-
-  dropdown.toggleClass('filled', !active);
-  dropdown.children('span').text(label);
-
-  if (!active) {
-    dropdown.find('option:contains(' + $(this).text() + ')').prop('selected', true);
-    $(this).parent().addClass('active');
-  }
-
-  dropdown.removeClass('open');
-});
-
-$('.dropdown > span').on('click touch', function (e) {
-  var self = $(this).parent();
-  self.toggleClass('open');
-});
-
-$(document).on('click touch', function (e) {
-  var dropdown = $('.dropdown');
-  if (dropdown !== e.target && !dropdown.has(e.target).length) {
-    dropdown.removeClass('open');
-  }
-});
-
-$(function () {
-  var $window = $(window);
-  var $sidebar = $(".sidebar");
-  var $sidebarTop = $sidebar.position().top;
-  var $sidebarHeight = $sidebar.height();
-  var $footer = $('.footer');
-  var $footerTop = $footer.position().top;
-
-  $window.scroll(function (event) {
-    $sidebar.addClass("fixed");
-    var $scrollTop = $window.scrollTop();
-    var $topPosition = Math.max(0, $sidebarTop - $scrollTop);
-
-    if ($scrollTop + $sidebarHeight > $footerTop) {
-      var $topPosition = Math.min($topPosition, $footerTop - $scrollTop - $sidebarHeight);
+jQuery(($) => {
+  $('.select').on('click', '.select__head', function () {
+    if ($(this).hasClass('open')) {
+      $(this).removeClass('open');
+      $(this).next().fadeOut();
+    } else {
+      $('.select__head').removeClass('open');
+      $('.select__list').fadeOut();
+      $(this).addClass('open');
+      $(this).next().fadeIn();
     }
+  });
 
-    $sidebar.css("top", $topPosition);
+  $('.select').on('click', '.select__item', function () {
+    $('.select__head').removeClass('open');
+    $(this).parent().fadeOut();
+    $(this).parent().prev().text($(this).text());
+    $(this).parent().prev().prev().val($(this).text());
+  });
+
+  $(document).click(function (e) {
+    if (!$(e.target).closest('.select').length) {
+      $('.select__head').removeClass('open');
+      $('.select__list').fadeOut();
+    }
   });
 });
+
+var sidebar = new StickySidebar('#sidebar', {
+  containerSelector: '#main-content',
+  innerWrapperSelector: '.sidebar__inner',
+  topSpacing: 20,
+
+});
+
 
 // Полифилы
 
